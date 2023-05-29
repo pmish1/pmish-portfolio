e2ea52b93be2a068a9bb8b2331af27f8d22aaadf
pmish <parshwa138@gmail.com>
2023-05-29 12:14:19 +1000

    added new website, updated resume

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,66 +1,129 @@
-import React from 'react'
+import React from "react"
 
-import html from '../assets/html.png'
-import css from '../assets/css.png'
-import javascript from '../assets/javascript.png'
-import mongo from '../assets/mongo.png'
-import react from '../assets/react.png'
-import tailwind from '../assets/tailwind.png'
-import node from '../assets/node.png'
-import firebase from '../assets/firebase.png'
+import html from "../assets/html.png"
+import css from "../assets/css.png"
+import javascript from "../assets/javascript.png"
+import mongo from "../assets/mongo.png"
+import react from "../assets/react.png"
+import tailwind from "../assets/tailwind.png"
+import node from "../assets/node.png"
+import firebase from "../assets/firebase.png"
+import typescript from "../assets/typescript.png"
+import nextjs from "../assets/Next.js.png"
+import github from "../assets/github.png"
 
 function Skills() {
-  return (
-    <div name="skills" className='bg-primary text-gray-300 w-full h-screen'>
-        {/* container */}
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div>
-                <p className='text-4xl text-gray-300 font-bold inline border-b-4 border-mainGreen'>Skills</p>
-                <p className='py-4'>These are the technologies I'm experienced with</p>
-            </div>
+    return (
+        <div name="skills" className="bg-primary text-gray-300 w-full h-screen">
+            {/* container */}
+            <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+                <div>
+                    <p className="text-4xl text-gray-300 font-bold inline border-b-4 border-mainGreen">
+                        Skills
+                    </p>
+                </div>
 
-            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                <div className=' hover:scale-110 duration-500'>
-                    <img src={html} alt="html icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>HTML</p>
+                <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 mt-10">
+                    <div className=" hover:scale-110 duration-500">
+                        <img
+                            src={javascript}
+                            alt="js icon"
+                            className="w-20 mx-auto"
+                        />
+                        <p className="my-4">JAVASCRIPT</p>
+                    </div>
+                    <div className=" hover:scale-110 duration-500">
+                        <img
+                            src={typescript}
+                            alt="firebase icon"
+                            className="w-20 mx-auto"
+                        />
+                        <p className="my-4">TYPESCRIPT</p>
+                    </div>
+                    <div className=" hover:scale-110 duration-500">
+                        <img
+                            src={html}
+                            alt="html icon"
+                            className="w-20 mx-auto"
+                        />
+                        <p className="my-4">HTML</p>
+                    </div>
+                    <div className=" hover:scale-110 duration-500">
+                        <img
+                            src={css}
+                            alt="css icon"
+                            className="w-20 mx-auto"
+                        />
+                        <p className="my-4">CSS</p>
+                    </div>
+                    <div className=" hover:scale-110 duration-500">
+                        <img
+                            src={react}
+                            alt="react icon"
+                            className="w-20 mx-auto"
+                        />
+                        <p className="my-4">REACT.js</p>
+                    </div>
+                    <div className=" hover:scale-110 duration-500">
+                        <img
+                            src={node}
+                            alt="node icon"
+                            className="w-20 mx-auto"
+                        />
+                        <p className="my-4">NODE.js</p>
+                    </div>
+                    <div className=" hover:scale-110 duration-500">
+                        <img
+                            src={nextjs}
+                            alt="nextjs icon"
+                            className="h-[90px] w-[125px] mx-auto"
+                        />
+                        <p className="my-1">NEXT.js</p>
+                    </div>
+                    <div className=" hover:scale-110 duration-500">
+                        <img
+                            src={mongo}
+                            alt="mongo icon"
+                            className="w-20 mx-auto"
+                        />
+                        <p className="my-4">MONGO_DB</p>
+                    </div>
+                    <div className=" hover:scale-110 duration-500">
+                        <img
+                            src={tailwind}
+                            alt="tailwind icon"
+                            className="w-20 mx-auto"
+                        />
+                        <p className="my-4">TAWILWIND_CSS</p>
+                    </div>
+
+                    <div className=" hover:scale-110 duration-500">
+                        <img
+                            src={firebase}
+                            alt="firebase icon"
+                            className="w-20 mx-auto"
+                        />
+                        <p className="my-4">FIREBASE</p>
+                    </div>
+
+                    <div className=" hover:scale-110 duration-500">
+                        <img
+                            src={github}
+                            alt="github icon"
+                            className="w-20 mx-auto"
+                        />
+                        <p className="my-4">GITHUB</p>
+                    </div>
                 </div>
-                <div className=' hover:scale-110 duration-500'>
-                    <img src={css} alt="css icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>CSS</p>
-                </div>
-                <div className=' hover:scale-110 duration-500'>
-                    <img src={javascript} alt="js icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>JAVASCRIPT</p>
-                </div>
-                <div className=' hover:scale-110 duration-500'>
-                    <img src={react} alt="react icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>REACT_JS</p>
-                </div>
-                <div className=' hover:scale-110 duration-500'>
-                    <img src={mongo} alt="mongo icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>MONGO_DB</p>
-                </div>
-                <div className=' hover:scale-110 duration-500'>
-                    <img src={tailwind} alt="tailwind icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>TAWILWIND_CSS</p>
-                </div>
-                <div className=' hover:scale-110 duration-500'>
-                    <img src={node} alt="node icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>NODE_JS</p>
-                </div>
-                <div className=' hover:scale-110 duration-500'>
-                    <img src={firebase} alt="firebase icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>FIREBASE</p>
+
+                <div>
+                    <p className="text-gray-300 mb-10 md:mb-0">
+                        I'm familiar with AWS S3, Python and PostgreSQL.
+                    </p>
                 </div>
             </div>
-
-            <div>
-                <p className='text-gray-300 mb-10 md:mb-0'>I'm also familiar with AWS S3 and Bootstrap </p>
-            </div>
-
         </div>
-    </div>
-  )
+    )
 }
 
 export default Skills
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,90 +1,182 @@
-import React from 'react'
-import chatapp from '../assets/chatappmish.png'
-import carrentals from '../assets/carrentals.png'
+import React from "react"
+import chatapp from "../assets/chatappmish.png"
+import carrentals from "../assets/carrentals.png"
+import similarityapi from "../assets/similarityapi.png"
 
 function Work() {
-  return (
-    <div className='bg-primary w-full md:h-screen text-gray-300' name="work">
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-mainGreen text-gray-300'>Work</p>
-                <p className='py-6'>Check out some of my recent work</p>
-            </div>
+    return (
+        <div
+            className="bg-primary w-full md:h-screen text-gray-300"
+            name="work"
+        >
+            <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+                <div className="pb-8">
+                    <p className="text-4xl font-bold inline border-b-4 border-mainGreen text-gray-300">
+                        Work
+                    </p>
+                    <p className="pt-6 ">
+                        This is what I've been up to recently.
+                    </p>
+                </div>
+                {/* ------------------------------------------------------------- */}
 
-
-
-
-
-            {/* card container */}
-            <div className='flex flex-col md:grid md:grid-cols-2 md:grid-rows-2'>
-                {/* car rentals  */}
-                <div 
-                    style={{backgroundImage: `url(${carrentals})`, objectFit: 'cover'}}
-                    className='group container rounded-sm flex justify-center items-center mx-auto content-div md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-2 md:mt-[5px]'
-                >                        
-
-                    {/* hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <div>
-                            <div className='pt-8 text-center'>
-                                <a href="https://main--gorgeous-yeot-dafbca.netlify.app/">
-                                    <button className='text-center rounded-sm px-4 py-3 m-2 bg-mainGreen text-black font-bold text-lg'>Demo</button>
-                                </a>
-                                <a href="https://github.com/pmish1/pmish-car-rentals-client">
-                                    <button className='text-center rounded-sm px-4 py-3 m-2 bg-mainGreen text-black font-bold text-lg'>Github</button>
-                                </a>
+                {/* card container */}
+                <div className="flex flex-col md:grid md:grid-cols-2 md:grid-rows-3 ">
+                    {/* ------------------------------------------------------------- */}
+                    {/* api service  */}
+                    <div
+                        style={{
+                            backgroundImage: `url(${similarityapi})`,
+                            objectFit: "contain",
+                        }}
+                        className="group container flex justify-center items-center mx-auto content-div md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-2 md:mt-[5px]"
+                    >
+                        {/* hover effects */}
+                        <div className="opacity-0 group-hover:opacity-100">
+                            <div>
+                                <div className="pt-8 text-center">
+                                    <a
+                                        href="https://similarity-api-pmish.vercel.app/"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        <button className="text-center px-4 py-3 m-2 bg-mainGreen text-black font-bold text-lg">
+                                            Demo
+                                        </button>
+                                    </a>
+                                    <a
+                                        href="https://github.com/pmish1/similarity-api-pmish"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        <button className="text-center px-4 py-3 m-2 bg-mainGreen text-black font-bold text-lg">
+                                            GitHub
+                                        </button>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                </div>
-                
-                {/* car rentals description */}
-                <div className='my-5 mx-2 md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-2 md:text-start md:mr-4 md:flex md:items-center'>
-                    <div>
-                        <h1 className='font-semibold mb-3 border-b border-mainGreen'>MERN Car Rental App</h1>
-                        <p>A full-stack application utilizing the MERN stack and responsive web design to facilitate listing management for a car-rental company. </p>
-                    </div>
-                </div>
-
-
-
-                {/* grid item */}
-                <div 
-                    style={{backgroundImage: `url(${chatapp})`}}
-                    className='group container rounded-sm flex justify-center items-center mx-auto content-div md:col-start-2 md:col-end-3 md:row-start-2 md:row-end-3'
-                >                        
-
-                    {/* hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
+                    {/* api service description */}
+                    <div className="my-5 mx-2 md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-2 md:text-start md:mr-4 md:flex md:items-center">
                         <div>
-                            <div className='pt-8 text-center'>
-                                <a href="https://whats-app-44493.web.app/">
-                                    <button className='text-center rounded-sm px-4 py-3 m-2 bg-mainGreen text-black font-bold text-lg'>Demo</button>
-                                </a>
-                                <a href="https://github.com/pmish1/chat-pmish">
-                                    <button className='text-center rounded-sm px-4 py-3 m-2 bg-mainGreen text-black font-bold text-lg'>Github</button>
-                                </a>
+                            <h1 className="font-semibold mb-3 border-b border-mainGreen">
+                                Full Stack API service
+                            </h1>
+                            <p>
+                                API service with rate-limited and protected
+                                routes which allows users submit text which are
+                                compared using OpenAI&apos;s Embeddings API and
+                                a Cosine Similarity algorithm to return a value
+                                describing the similarity between the texts.{" "}
+                            </p>
+                        </div>
+                    </div>
+                    {/* ------------------------------------------------------------- */}
+
+                    {/* car rentals  */}
+                    <div
+                        style={{
+                            backgroundImage: `url(${carrentals})`,
+                            objectFit: "cover",
+                        }}
+                        className="group container flex justify-center items-center mx-auto content-div md:col-start-2 md:col-end-3 md:row-start-2 md:row-end-3 md:mt-[5px]"
+                    >
+                        {/* hover effects */}
+                        <div className="opacity-0 group-hover:opacity-100">
+                            <div>
+                                <div className="pt-8 text-center">
+                                    <a
+                                        href="https://main--gorgeous-yeot-dafbca.netlify.app/"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        <button className="text-center px-4 py-3 m-2 bg-mainGreen text-black font-bold text-lg">
+                                            Demo
+                                        </button>
+                                    </a>
+                                    <a
+                                        href="https://github.com/pmish1/pmish-car-rentals-client"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        <button className="text-center px-4 py-3 m-2 bg-mainGreen text-black font-bold text-lg">
+                                            GitHub
+                                        </button>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                </div>
+                    {/* car rentals description */}
+                    <div className="my-5 mx-2 md:col-start-1 md:col-end-2 md:row-start-2 md:row-end-3 md:text-end md:mr-4 md:flex md:items-center">
+                        <div>
+                            <h1 className="font-semibold mb-3 border-b border-mainGreen">
+                                MERN Car Rental App
+                            </h1>
+                            <p>
+                                A full-stack application utilizing the MERN
+                                stack and responsive web design to facilitate
+                                listing management for a car-rental company.{" "}
+                            </p>
+                        </div>
+                    </div>
 
-                {/* chat app description */}
-                <div className='my-5 mx-2 md:col-start-1 md:col-end-2 md:row-start-2 md:row-end-3 md:text-end md:pl-2 md:flex md:items-center'>
-                    <div>
-                        <h1 className='font-semibold mb-3 border-b border-mainGreen'>React Chat App</h1>
-                        <p>A real-time web-based chat application, equipped with user authentication and authorization functionalities.</p>
+                    {/* ------------------------------------------------------------- */}
+
+                    {/* chat app */}
+                    <div
+                        style={{ backgroundImage: `url(${chatapp})` }}
+                        className="group container flex justify-center items-center mx-auto content-div md:col-start-1 md:col-end-2 md:row-start-3 md:row-end-4"
+                    >
+                        {/* hover effects */}
+                        <div className="opacity-0 group-hover:opacity-100">
+                            <div>
+                                <div className="pt-8 text-center">
+                                    <a
+                                        href="https://whats-app-44493.web.app/"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        <button className="text-center px-4 py-3 m-2 bg-mainGreen text-black font-bold text-lg">
+                                            Demo
+                                        </button>
+                                    </a>
+                                    <a
+                                        href="https://github.com/pmish1/chat-pmish"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        <button className="text-center px-4 py-3 m-2 bg-mainGreen text-black font-bold text-lg">
+                                            GitHub
+                                        </button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* chat app description */}
+                    <div className="my-5 mx-2 md:col-start-2 md:col-end-3 md:row-start-3 md:row-end-4 md:text-start md:flex md:items-center">
+                        <div>
+                            <h1 className="font-semibold mb-3 border-b border-mainGreen">
+                                React Chat App
+                            </h1>
+                            <p>
+                                A real-time web-based chat application, equipped
+                                with user authentication and authorization
+                                functionalities.
+                            </p>
+                        </div>
                     </div>
                 </div>
-
-
+                {/* ------------------------------------------------------------- */}
             </div>
-
+            {/* ------------------------------------------------------------- */}
         </div>
-    </div>
-  )
+    )
 }
 
 export default Work
